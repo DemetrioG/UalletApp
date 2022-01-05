50b4539cf4187d2fa962d6715acfc7766aae2585
DemetrioG <demetrio.gabriel@hotmail.com>
2022-01-05 20:36:28 -0300

    Libs e Navegações

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image, Platform } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './src/routes';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useFonts } from 'expo-font';
+import SafeAreaView from 'react-native-safe-area-view';
+import { general } from './src/styles';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  let [fontLoaded] = useFonts ({
+    'RalewayMedium' : require('./assets/fonts/Raleway-Medium.ttf'),
+    'RalewayBold' : require('./assets/fonts/Raleway-Bold.ttf'),
+    'RalewayExtraBold' : require('./assets/fonts/Raleway-ExtraBold.ttf'),
+    'MontserratMedium' : require('./assets/fonts/Montserrat-Medium.ttf'),
+    'MontserratBold' : require('./assets/fonts/Montserrat-Bold.ttf'),
+    'MontserratExtraBold' : require('./assets/fonts/Montserrat-ExtraBold.ttf'),
+  })
+
+  if (!fontLoaded) {
+    return (
+      <Image
+        source={require('./assets/images/splash.png')}
+      />
+    )
+  } else {
+    return (
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <SafeAreaView style={[general.flex, general.statusBar]}>
+            <Routes/>
+          </SafeAreaView>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
