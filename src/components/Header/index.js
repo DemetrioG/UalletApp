bcdb64513e3ac3b0d5192033f8e12848ab902a6d
DemetrioG <demetrio.gabriel@hotmail.com>
2022-02-01 17:22:15 -0300

    Implantação de menu no Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Platform } from 'react-native';
 import { connect } from 'react-redux';
 import Feather from 'react-native-vector-icons/Feather';
 
 import firebase from '../../services/firebase';
 import styles from './styles';
-import { metrics, colors } from '../../styles';
+import { metrics, colors, general } from '../../styles';
 import { editName } from '../Actions/nameAction';
 import { editMonth } from '../Actions/monthAction';
 import { editYear } from '../Actions/yearAction';
+import { editModality } from '../Actions/modalityAction';
 import ModalPicker from '../ModalPicker';
 
 export function Header(props) {
+
+    const [menu, setMenu] = useState(false);
+    const index = { zIndex: 5 };
 
     const optionsMonth = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
     const optionsYear = [];
@@ -37,8 +41,12 @@ export function Header(props) {
         }
     }, []);
 
+    function changeModality() {
+        props.editModality(props.modality == 'Real' ? 'Projetado' : 'Real');
+    }
+
     return (
-        <View style={styles().headerView}>
+        <View style={[styles().headerView, Platform.OS === 'ios' ? index : null]}>
             <ModalPicker
                 options={optionsMonth}
                 selectedValue={props.editMonth}
@@ -66,10 +74,33 @@ export function Header(props) {
                 <TouchableOpacity style={styles().spaceIcon}>
                     <Feather name='bell' size={metrics.iconSize} color={props.theme == 'light' ? colors.darkPrimary : colors.white}/>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles().spaceIcon}>
+                <TouchableOpacity style={styles().spaceIcon} onPress={() => menu ? setMenu(false) : setMenu(true)}>
                     <Feather name='more-horizontal' size={metrics.iconSize} color={props.theme == 'light' ? colors.darkPrimary : colors.white}/>
                 </TouchableOpacity>
             </View>
+            {
+                menu &&
+                <View style={styles().menuView}>
+                    <View style={styles().itemView}>
+                        <Text style={styles().itemText}>{props.modality}</Text>
+                        <TouchableOpacity style={styles().smallIconPadding} onPress={changeModality}>
+                            <Feather name='refresh-cw' size={15} color={colors.white}/>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles().itemView}>
+                        <Text style={styles().itemText}>Configurações</Text>
+                        <TouchableOpacity style={styles().smallIconPadding}>
+                            <Feather name='settings' size={15} color={colors.white}/>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles().itemView}>
+                        <TouchableOpacity style={styles().logoutButton}>
+                            <Text style={styles().logoutText}>LOGOUT</Text>
+                            <Feather name='power' size={15} color={colors.lightRed}/>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            }
         </View>
     );
 }
@@ -80,10 +111,11 @@ const mapStateToProps = (state) => {
         uid : state.user.uid,
         name: state.name.name,
         month: state.date.month,
-        year: state.date.year
+        year: state.date.year,
+        modality: state.modality.modality
     }
   }
   
-const headerConnect = connect(mapStateToProps, { editName, editMonth, editYear })(Header);
+const headerConnect = connect(mapStateToProps, { editName, editMonth, editYear, editModality })(Header);
 
 export default headerConnect;
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -7,7 +7,7 @@ const styles = (theme) => StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-between',
         marginBottom: metrics.baseMargin,
-        paddingLeft: metrics.basePadding
+        paddingLeft: metrics.basePadding,
     },
     headerText: {
         fontFamily: fonts.ralewayMedium,
@@ -19,6 +19,43 @@ const styles = (theme) => StyleSheet.create({
     },
     spaceIcon: {
         marginLeft: metrics.baseMargin
+    },
+    smallIconPadding: {
+        paddingHorizontal: 10
+    },
+    menuView: {
+        padding: metrics.basePadding,
+        position: 'absolute',
+        right: -5,
+        top: 40,
+        backgroundColor: colors.infoBlack,
+        borderRadius: metrics.baseRadius,
+        width: 200,
+        zIndex: 5,
+    },
+    itemView: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingVertical: metrics.basePadding,
+        paddingHorizontal: metrics.basePadding - 5,
+        borderBottomWidth: 1,
+        borderColor: colors.gray
+    },
+    itemText: {
+        fontFamily: fonts.ralewayMedium,
+        fontSize: fonts.medium,
+        color: colors.white
+    },
+    logoutText: {
+        fontFamily: fonts.ralewayBold,
+        fontSize: fonts.regular,
+        color: colors.lightRed,
+        marginRight: metrics.baseMargin
+    },
+    logoutButton: {
+        flexDirection: 'row',
+        alignItems: 'center'
     }
 });
 
