3190050a0473cc916b28b08bca4e512445cd267b
DemetrioG <demetrio.gabriel@hotmail.com>
2022-04-04 22:32:47 -0300

    feat: prettier

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,27 +1,42 @@
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaView } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Index from '../pages/Index';
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import ForgotPassword from '../pages/ForgotPassword';
-import { general } from '../styles';
+import Index from "../pages/Index";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import ForgotPassword from "../pages/ForgotPassword";
+import { general } from "../styles";
+import { SafeAreaContainer } from "../styles/general";
 
 const Stack = createNativeStackNavigator();
 export default function AuthRoutes() {
-    
-    return (
-        <NavigationContainer>
-            <SafeAreaView style={general().flex}>
-                <Stack.Navigator>
-                    <Stack.Screen name="Index" component={Index} options={{headerShown: false}}/>
-                    <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
-                    <Stack.Screen name="Register" component={Register} options={{headerShown: false}}/>
-                    <Stack.Screen name="Forgot" component={ForgotPassword} options={{headerShown: false}}/>
-                </Stack.Navigator>
-            </SafeAreaView>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <SafeAreaContainer style={general().flex}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Index"
+            component={Index}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={Register}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Forgot"
+            component={ForgotPassword}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </SafeAreaContainer>
+    </NavigationContainer>
+  );
 }
