bc0b8781c3f7d92a848b1b7b25431f7e9b860149
DemetrioG <demetrio.gabriel@hotmail.com>
2022-04-13 14:31:30 -0300

    feat: função para tratar com AsyncStorage

---
+++ src/functions/storageData.ts
@@ -0,0 +1,10 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export async function setStorage(key: string, data: object) {
+  await AsyncStorage.setItem(key, JSON.stringify(data));
+}
+
+export async function getStorage(key: string) {
+  const response = (await AsyncStorage.getItem(key)) || "";
+  return JSON.parse(response);
+}
