e3a2b5b66193331870f22efa7405ccefcb87d3ef
DemetrioG <demetrio.gabriel@hotmail.com>
2022-06-21 11:44:05 -0300

    feat: listener para conexão com internet

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
+import NetInfo from "@react-native-community/netinfo";
 import AuthRoutes from "./authRoutes";
 import AppRoutes from "./appRoutes";
 import { UserContext } from "../context/User/userContext";
 import { getStorage, removeAllStorage } from "../utils/storage.helper";
+import { DataContext } from "../context/Data/dataContext";
 
 export default function routes() {
+  const { setData } = React.useContext(DataContext);
   const { user, setUser } = React.useContext(UserContext);
 
   React.useEffect(() => {
-    async function loadStorage() {
+    (async () => {
       const storageUser = await getStorage("authUser");
       const storageDate = Date.parse(storageUser?.date);
       const today = Date.parse(new Date(Date.now()).toString());
@@ -23,9 +26,14 @@ export default function routes() {
       }
 
       return removeAllStorage();
-    }
+    })();
 
-    loadStorage();
+    NetInfo.addEventListener(({ isConnected }) => {
+      setData((dataState) => ({
+        ...dataState,
+        isNetworkConnected: isConnected,
+      }));
+    });
   }, []);
 
   return user.signed ? <AppRoutes /> : <AuthRoutes />;
