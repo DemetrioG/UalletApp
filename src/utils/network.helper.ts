095db13e5d33cd08db8dbc2984602c08221be2d1
DemetrioG <demetrio.gabriel@hotmail.com>
2022-06-21 11:44:23 -0300

    feat: função para alertar falha de conexão

---
+++ src/utils/network.helper.ts
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { IAlert } from "../context/Alert/alertContext";
+
+export function networkConnection(
+  isConnected: boolean,
+  alertContext: React.Dispatch<React.SetStateAction<IAlert>>
+) {
+  if (!isConnected) {
+    return alertContext(() => ({
+      title: "Falha na conexão com a internet",
+      type: "network",
+      helperText:
+        "Reestabeleça a conexão para prosseguir com os lançamentos e consultas!",
+      visibility: true,
+    }));
+  }
+}
