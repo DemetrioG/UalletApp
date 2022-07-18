79724aa66e6e915c41ce2057522b80a3d831db12
Matheus Carvalho <matheusfelipe_contato@hotmail.com>
2022-07-18 08:55:50 -0300

    feat: link para referencia regex

--- src/screens/Register/PasswordRules.tsx
+++ src/screens/Register/PasswordRules.tsx
@@ -9,6 +9,16 @@ const dataRules = [
     "Oito caracteres",
 ];
 
+/**
+ * https://www.ocpsoft.org/tutorials/regular-expressions/password-regular-expression/
+  ^                                            Match the beginning of the string
+  (?=.*[0-9])                                  Require that at least one digit appear anywhere in the string
+  (?=.*[a-z])                                  Require that at least one lowercase letter appear anywhere in the string
+  (?=.*[A-Z])                                  Require that at least one uppercase letter appear anywhere in the string
+  (?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\])        Require that at least one special character appear anywhere in the string
+  .{8,32}                                      The password must be at least 8 characters long, but no more than 32
+  $                                            Match the end of the string.
+ */
 const Regex = new RegExp("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\]).{8,32}$");
 
 function checkPassword(passwordText: string) : number {
