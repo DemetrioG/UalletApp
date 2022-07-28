8659f0da9cd45cf15cdcf4288ca8fbd7b7d508f4
DemetrioG <demetrio.gabriel@hotmail.com>
2022-07-28 17:49:09 -0300

    fix: ajuste de titulo da screen

--- src/screens/Configurations/DadosCadastrais/index.tsx
+++ src/screens/Configurations/DadosCadastrais/index.tsx
@@ -3,42 +3,46 @@ import { HStack } from "native-base";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import Icon from "../../../components/Icon";
 import { ItemContainer, ItemText } from "../../../components/Menu/styles";
-import { fonts } from "../../../styles";
-import { BackgroundContainer, TextHeader, ViewTab } from "../../../styles/general";
+import {
+  BackgroundContainer,
+  TextHeaderScreen,
+  ViewTab,
+} from "../../../styles/general";
 
 const actions = [
-  {label: "Informações Pessoais", url: "Configuracoes/DadosCadastrais/InformacoesCadastrais"},
-  {label: "Alterar senha", url: ""},
-]
+  {
+    label: "Informações Pessoais",
+    url: "Configuracoes/DadosCadastrais/InformacoesCadastrais",
+  },
+  { label: "Alterar senha", url: "" },
+];
 
 export const DadosCadastraisScreen = () => {
   const { goBack, navigate } = useNavigation();
 
-  function goTo (url: string) {
+  function goTo(url: string) {
     //TODO: tipagem de rota
     navigate(url as any);
-  };
+  }
 
   return (
     <BackgroundContainer>
       <ViewTab>
         <HStack alignItems="center" space={3} mb={10}>
-            <Icon name="chevron-left" size={24} onPress={goBack}/>
-            <TextHeader fontSize={fonts.large}>Dados Cadastrais</TextHeader>
+          <Icon name="chevron-left" size={24} onPress={goBack} />
+          <TextHeaderScreen noMarginBottom>Dados Cadastrais</TextHeaderScreen>
         </HStack>
-        {
-          actions.map((action) => (
-            <TouchableOpacity onPress={() => goTo(action.url)}>
-              <ItemContainer>
-                <HStack justifyContent="space-between" flex={1}>
-                    <ItemText>{action.label}</ItemText>
-                  <Icon name="chevron-right" size={18} />
-                </HStack>
-              </ItemContainer>
+        {actions.map((action) => (
+          <TouchableOpacity onPress={() => goTo(action.url)}>
+            <ItemContainer>
+              <HStack justifyContent="space-between" flex={1}>
+                <ItemText>{action.label}</ItemText>
+                <Icon name="chevron-right" size={18} />
+              </HStack>
+            </ItemContainer>
           </TouchableOpacity>
-          ))
-        }
+        ))}
       </ViewTab>
     </BackgroundContainer>
-  )
+  );
 };
--- src/screens/Configurations/index.tsx
+++ src/screens/Configurations/index.tsx
@@ -3,46 +3,56 @@ import { HStack } from "native-base";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import Icon from "../../components/Icon";
 import { ItemContainer, ItemText } from "../../components/Menu/styles";
-import { fonts } from "../../styles";
-import { BackgroundContainer, TextHeader, ViewTab } from "../../styles/general";
+import {
+  BackgroundContainer,
+  TextHeaderScreen,
+  ViewTab,
+} from "../../styles/general";
 
 const actions = [
-  {label: "Dados Cadastrais", url: "Configuracoes/DadosCadastrais", icon: "user", color: "blue"},
-]
+  {
+    label: "Dados Cadastrais",
+    url: "Configuracoes/DadosCadastrais",
+    icon: "user",
+    color: "blue",
+  },
+];
 
 function ConfiguracoesScreen() {
   const { goBack, navigate } = useNavigation();
 
-  function goTo (url: string) {
+  function goTo(url: string) {
     //TODO: tipagem de rota
     navigate(url as any);
-  };
+  }
 
   return (
     <BackgroundContainer>
       <ViewTab>
         <HStack alignItems="center" space={3} mb={10}>
-            <Icon name="chevron-left" size={24} onPress={goBack}/>
-            <TextHeader fontSize={fonts.large}>Configurações</TextHeader>
+          <Icon name="chevron-left" size={24} onPress={goBack} />
+          <TextHeaderScreen noMarginBottom>Configurações</TextHeaderScreen>
         </HStack>
-        {
-          actions.map((action) => (
-            <TouchableOpacity onPress={() => goTo(action.url)} key={action.url}>
-              <ItemContainer>
-                <HStack justifyContent="space-between" flex={1}>
-                  <HStack alignItems="center" space={3}>
-                    <Icon colorVariant={action.color} name={action.icon} size={24} />
-                    <ItemText>{action.label}</ItemText>
-                  </HStack>
-                  <Icon name="chevron-right" size={18} />
+        {actions.map((action) => (
+          <TouchableOpacity onPress={() => goTo(action.url)} key={action.url}>
+            <ItemContainer>
+              <HStack justifyContent="space-between" flex={1}>
+                <HStack alignItems="center" space={3}>
+                  <Icon
+                    colorVariant={action.color}
+                    name={action.icon}
+                    size={24}
+                  />
+                  <ItemText>{action.label}</ItemText>
                 </HStack>
-              </ItemContainer>
+                <Icon name="chevron-right" size={18} />
+              </HStack>
+            </ItemContainer>
           </TouchableOpacity>
-          ))
-        }
+        ))}
       </ViewTab>
     </BackgroundContainer>
-  )
+  );
 }
 
 export default ConfiguracoesScreen;
