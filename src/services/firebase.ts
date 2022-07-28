fe59aa808812510388c064ff6710417d378942e9
DemetrioG <demetrio.gabriel@hotmail.com>
2022-07-28 18:00:02 -0300

    fix: correção de arquivos

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,15 +1,24 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import {
+  API_KEY,
+  APP_ID,
+  AUTH_DOMAIN,
+  MEASUREMENT_ID,
+  MESSAGING_SENDER_ID,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+} from "@env";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRDE67-4oPYZN3-jIpKNfTyi2776Mj7Go",
-  authDomain: "uallet-65789.firebaseapp.com",
-  projectId: "uallet-65789",
-  storageBucket: "uallet-65789.appspot.com",
-  messagingSenderId: "1000376585",
-  appId: "1:1000376585:web:9abac518451c2f65b2abd8",
-  measurementId: "G-1W6ETSJ9B1"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 // Initialize Firebase
