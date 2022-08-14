fca25878d99d43bc9b559d0d5c9f00fa4d1d5867
DemetrioG <demetrio.gabriel@hotmail.com>
2022-08-13 22:38:07 -0300

    fix: ajustes gerais

--- src/screens/Configurations/DadosCadastrais/AlterarSenha/index.tsx
+++ src/screens/Configurations/DadosCadastrais/AlterarSenha/index.tsx
@@ -9,121 +9,113 @@ import * as yup from "yup";
 import Icon from "../../../../components/Icon";
 import TextInputPassword from "../../../../components/TextInputPassword";
 import {
-    BackgroundContainer,
-    ButtonText,
-    FormContainer,
-    StyledKeyboardAvoidingView,
-    TextHeaderScreen,
-    ViewTab,
+  BackgroundContainer,
+  ButtonText,
+  ContainerCenter,
+  FormContainer,
+  StyledKeyboardAvoidingView,
+  TextHeaderScreen,
+  ViewTab,
 } from "../../../../styles/general";
 import PasswordRules from "../../../Register/PasswordRules";
 import { changePassword } from "./querys";
 import { AlertContext } from "../../../../context/Alert/alertContext";
 
 const defaultValues = {
-    oldPassword: "",
-    newPassword: "",
+  oldPassword: "",
+  newPassword: "",
 };
 
 const schema = yup
-    .object({
-        newPassword: yup
-            .string()
-            .required()
-            .matches(
-                new RegExp(
-                    "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
-                )
-            ),
-        oldPassword: yup.string().required(),
-    })
-    .required();
+  .object({
+    newPassword: yup
+      .string()
+      .required()
+      .matches(
+        new RegExp(
+          "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
+        )
+      ),
+    oldPassword: yup.string().required(),
+  })
+  .required();
 
 export const AlterarSenhaScreen = () => {
-    const [loading, setLoading] = React.useState(false);
-    const { setAlert } = React.useContext(AlertContext);
-    const { goBack, navigate } = useNavigation();
-    const {
-        control,
-        handleSubmit,
-        formState: { errors },
-        setError,
-        watch,
-    } = useForm({
-        resolver: yupResolver(schema),
-        defaultValues,
-    });
+  const [loading, setLoading] = React.useState(false);
+  const { setAlert } = React.useContext(AlertContext);
+  const { goBack, navigate } = useNavigation();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    watch,
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues,
+  });
 
-    async function onSubmit(formValues: typeof defaultValues) {
-        setLoading(true);
+  async function onSubmit(formValues: typeof defaultValues) {
+    setLoading(true);
 
-        const changed = await changePassword(formValues);
+    const changed = await changePassword(formValues);
 
-        if (!changed) {
-            setAlert({
-                type: "error",
-                title: "A senha antiga é inválida",
-                callback: true,
-                callbackFunction: () =>
-                    setError(
-                        "oldPassword",
-                        { message: "error", type: "value" },
-                    ),
-            });
-            return setLoading(false);
-        }
-
-        return setAlert({
-            title: "Senha alterada com sucesso",
-            redirect: "Home",
-        });
+    if (!changed) {
+      setAlert({
+        type: "error",
+        title: "A senha antiga é inválida",
+        callback: true,
+        callbackFunction: () =>
+          setError("oldPassword", { message: "error", type: "value" }),
+      });
+      return setLoading(false);
     }
 
-    return (
-        <StyledKeyboardAvoidingView>
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <BackgroundContainer>
-                    <ViewTab>
-                        <HStack alignItems="center" space={3} mb={10}>
-                            <Icon
-                                name="chevron-left"
-                                size={24}
-                                onPress={goBack}
-                            />
-                            <TextHeaderScreen noMarginBottom>
-                                Alterar Senha
-                            </TextHeaderScreen>
-                        </HStack>
-                        <VStack flex={1} justifyContent="center">
-                            <FormContainer>
-                                <TextInputPassword
-                                    placeholder="Senha atual *"
-                                    name="oldPassword"
-                                    returnKeyType="done"
-                                    control={control}
-                                    errors={errors.oldPassword}
-                                />
-                                <TextInputPassword
-                                    placeholder="Nova senha *"
-                                    returnKeyType="done"
-                                    name="newPassword"
-                                    control={control}
-                                    errors={errors.newPassword}
-                                    helperText="Informe todos os campos"
-                                />
-                                <PasswordRules content={watch("newPassword")} />
-                                <Button
-                                    mt={5}
-                                    isLoading={loading}
-                                    onPress={handleSubmit(onSubmit)}
-                                >
-                                    <ButtonText>ALTERAR SENHA</ButtonText>
-                                </Button>
-                            </FormContainer>
-                        </VStack>
-                    </ViewTab>
-                </BackgroundContainer>
-            </TouchableWithoutFeedback>
-        </StyledKeyboardAvoidingView>
-    );
+    return setAlert({
+      title: "Senha alterada com sucesso",
+      redirect: "Home",
+    });
+  }
+
+  return (
+    <StyledKeyboardAvoidingView>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <BackgroundContainer>
+          <ViewTab>
+            <HStack alignItems="center" space={3} mb={10}>
+              <Icon name="chevron-left" size={24} onPress={goBack} />
+              <TextHeaderScreen noMarginBottom>Alterar Senha</TextHeaderScreen>
+            </HStack>
+            <ContainerCenter>
+              <FormContainer insideApp>
+                <TextInputPassword
+                  placeholder="Senha atual *"
+                  name="oldPassword"
+                  returnKeyType="done"
+                  control={control}
+                  errors={errors.oldPassword}
+                />
+                <TextInputPassword
+                  placeholder="Nova senha *"
+                  returnKeyType="done"
+                  name="newPassword"
+                  control={control}
+                  errors={errors.newPassword}
+                  helperText="Informe todos os campos"
+                />
+                <PasswordRules content={watch("newPassword")} />
+                <Button
+                  mt={5}
+                  isLoading={loading}
+                  onPress={handleSubmit(onSubmit)}
+                >
+                  <ButtonText>ALTERAR SENHA</ButtonText>
+                </Button>
+              </FormContainer>
+            </ContainerCenter>
+          </ViewTab>
+        </BackgroundContainer>
+      </TouchableWithoutFeedback>
+    </StyledKeyboardAvoidingView>
+  );
 };
