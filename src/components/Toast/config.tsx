00855dba75f683a86608aec53587b42e66b8150d
DemetrioG <demetrio.gabriel@hotmail.com>
2022-08-18 18:55:52 -0300

    feat: configuração do novo toast alert

---
+++ src/components/Toast/config.tsx
@@ -0,0 +1,49 @@
+import { BaseToast } from "react-native-toast-message";
+import styled from "styled-components";
+import { colors, fonts, metrics } from "../../styles";
+import DefaultIcon from "../Icon";
+
+const Icon = styled(DefaultIcon)`
+  top: 0;
+  bottom: 0;
+  left: 10px;
+  margin-top: auto;
+  margin-bottom: auto;
+`;
+
+const StyledToastSuccess = styled(BaseToast).attrs(({ theme: { theme } }) => ({
+  contentContainerStyle: {
+    paddingHorizontal: metrics.basePadding,
+  },
+  text1Style: {
+    fontSize: fonts.medium,
+    fontFamily: fonts.ralewayBold,
+    color: theme.isOnDarkTheme ? colors.darkPrimary : colors.white,
+    marginTop: 3,
+  },
+}))`
+  top: -5px;
+  border-left-color: #47bf2f;
+  background-color: ${({ theme: { theme } }) =>
+    theme.isOnDarkTheme ? colors.lightPrimary : colors.darkSecondary};
+`;
+
+const StyledToastError = styled(StyledToastSuccess)`
+  border-left-color: ${colors.strongRed};
+`;
+
+export const toastConfig = {
+  success: (props: any) => (
+    <StyledToastSuccess
+      {...props}
+      renderLeadingIcon={() => <Icon name="check" color="#47bf2f" />}
+    />
+  ),
+
+  error: (props: any) => (
+    <StyledToastError
+      {...props}
+      renderLeadingIcon={() => <Icon name="x" color={colors.strongRed} />}
+    />
+  ),
+};
