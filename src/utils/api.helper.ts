62edcd956317e84fd5818ffdf0cd97f9a7b5a167
DemetrioG <demetrio.gabriel@hotmail.com>
2022-08-23 23:12:15 -0300

    feat: nova url

--- src/utils/api.helper.ts
+++ src/utils/api.helper.ts
@@ -16,6 +16,7 @@ export const STATUS_INVEST_BASE_URL = 'https://statusinvest.com.br/';
 export const TESOURO_URL = 'tesouro/';
 export const ACOES_URL = 'acoes/';
 export const FII_URL = 'fiis/';
+export const CRIPTO_URL = 'cripto?ticker=';
 
 const axios = defaultAxios.create({
     baseURL: AWS_WEBSERVICE_ENDPOINT.toString()
