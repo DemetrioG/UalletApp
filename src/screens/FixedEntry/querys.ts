850eff2acef3a82dbc48cedb09ddf7bd1df806f4
Stags <matheusfelipe_contato@hotmail.com>
2022-08-28 17:27:07 -0300

    feat: fixed entry

---
+++ src/screens/FixedEntry/querys.ts
@@ -0,0 +1,89 @@
+import firebase from "firebase";
+import { string } from "yup";
+
+type LastIdFromEntryProps = { modality: string }
+export const lastIdFromEntry = async ({ modality }: LastIdFromEntryProps) => {
+  const user = firebase.auth().currentUser;
+
+  if (!user) return -1;
+
+  let id = 0;
+
+  const results = await firebase
+    .firestore()
+    .collection("entry")
+    .doc(user.uid)
+    .collection(modality!)
+    .orderBy("id", "desc")
+    .limit(1)
+    .get()
+
+  results.forEach((result) => {
+    id += result.data().id;
+  });
+
+  return id;
+}
+
+
+type InserNewEntryProps = {
+  modality: string,
+  id: number,
+  date: firebase.firestore.Timestamp,
+  type: string,
+  description: string,
+  segment: string,
+  value: number
+}
+
+export const insertNewEntry = async ({ modality, ...props }: InserNewEntryProps) => {
+  const user = firebase.auth().currentUser;
+
+  if (!user) return Promise.resolve(false);
+
+  try {
+    await firebase.firestore()
+      .collection("entry")
+      .doc(user.uid)
+      .collection(modality!)
+      .doc(props.id.toString())
+      .set(props);
+    Promise.resolve(true);
+  }
+  catch (e) {
+    console.error(e);
+    Promise.reject(false);
+  }
+}
+
+type UpdateCurrentBalanceProps = { modality: string, docDate: string, value: number }
+export const updateCurrentBalance = ({ modality, docDate, value }: UpdateCurrentBalanceProps) => {
+  const user = firebase.auth().currentUser;
+
+  if (!user) return Promise.resolve(false);
+
+  let balance = 0;
+  firebase
+    .firestore()
+    .collection("balance")
+    .doc(user.uid)
+    .collection(modality!)
+    .doc(docDate)
+    .get()
+    .then((v) => {
+      balance = v.data()?.balance || 0;
+    }).catch(() => Promise.reject(false));
+
+  balance -= value;
+
+  firebase
+    .firestore()
+    .collection("balance")
+    .doc(user.uid)
+    .collection(modality!)
+    .doc(docDate)
+    .set({
+      balance: balance,
+    }).then(() => Promise.resolve(true))
+    .catch(() => Promise.resolve(false));
+}
