9a26e814f5a5d928b8223cd881e5348a844a21fa
DemetrioG <demetrio.gabriel@hotmail.com>
2022-08-31 10:13:04 -0300

    feat: spinner e loader nos investimentos

--- src/screens/Investments/index.tsx
+++ src/screens/Investments/index.tsx
@@ -9,28 +9,47 @@ import {
   BackgroundContainer,
   Balance,
   ButtonText,
+  Skeleton,
   TextHeaderScreen,
   ViewTab,
 } from "../../styles/general";
 import { metrics } from "../../styles";
-import { PatrimonyText } from "./styles";
+import { PatrimonyText, Spinner } from "./styles";
+import { LoaderContext } from "../../context/Loader/loaderContext";
 
 const Investments = () => {
   const { navigate } = useNavigation<NativeStackNavigationProp<any>>();
+  const {
+    loader: { positions, equity, investVisible },
+    setLoader,
+  } = React.useContext(LoaderContext);
   const [totalEquity, setTotalEquity] = React.useState(0);
+  const [spinner, setSpinner] = React.useState(true);
+
+  React.useEffect(() => {
+    if (equity && positions) {
+      setLoader((state) => ({
+        ...state,
+        investVisible: false,
+      }));
+    }
+  }, [equity, positions]);
 
   return (
     <BackgroundContainer>
       <ViewTab>
         <ScrollView showsVerticalScrollIndicator={false}>
-          <HStack mb={metrics.baseMargin}>
-            <TextHeaderScreen>Investimentos</TextHeaderScreen>
+          <HStack mb={metrics.baseMargin} justifyContent={"space-between"}>
+            <TextHeaderScreen noMarginBottom>Investimentos</TextHeaderScreen>
+            {spinner && <Spinner />}
           </HStack>
           <PatrimonyText>Patrimônio investido</PatrimonyText>
           <VStack mt={1}>
-            <Balance>{numberToReal(totalEquity)}</Balance>
+            <Skeleton isLoaded={!investVisible} mt={2}>
+              <Balance>{numberToReal(totalEquity)}</Balance>
+            </Skeleton>
           </VStack>
-          <Positions setTotalEquity={setTotalEquity} />
+          <Positions setTotalEquity={setTotalEquity} setSpinner={setSpinner} />
           <Button mt={10} onPress={() => navigate("NovoAtivo")}>
             <ButtonText>ADICIONAR ATIVO</ButtonText>
           </Button>
