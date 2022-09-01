f4b34b15c433891c0196a716a03477b9846152e0
DemetrioG <demetrio.gabriel@hotmail.com>
2022-09-01 09:30:14 -0300

    feat: criação do tooltip padrão para o app

---
+++ src/components/Tooltip/index.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { Text } from "./styles";
+import { TouchableOpacity } from "react-native";
+import NativeTooltip from "react-native-walkthrough-tooltip";
+import { colors, metrics } from "../../styles";
+import { useTheme } from "styled-components";
+import { IThemeProvider } from "../../../App";
+
+const Tooltip = ({
+  text,
+  children,
+}: {
+  text: string;
+  children: JSX.Element;
+}) => {
+  const [open, setOpen] = React.useState(false);
+
+  const { theme }: IThemeProvider = useTheme();
+
+  return (
+    <NativeTooltip
+      isVisible={open}
+      content={<Text>{text}</Text>}
+      onClose={() => setOpen(false)}
+      backgroundColor="transparent"
+      disableShadow
+      placement="bottom"
+      tooltipStyle={{
+        marginLeft: 7,
+      }}
+      contentStyle={{
+        backgroundColor: theme?.isOnDarkTheme
+          ? colors.transpLight
+          : colors.transpDark,
+        borderRadius: metrics.smallRadius,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <TouchableOpacity onPress={() => setOpen(!open)}>
+        {children}
+      </TouchableOpacity>
+    </NativeTooltip>
+  );
+};
+
+export default Tooltip;
