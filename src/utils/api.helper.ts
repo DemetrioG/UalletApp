0a7f82553d388abb415aec7077e07d2272571139
DemetrioG <demetrio.gabriel@hotmail.com>
2022-09-01 09:41:13 -0300

    fix: exclusão de funções não utilizadas

--- src/utils/api.helper.ts
+++ src/utils/api.helper.ts
@@ -1,25 +1,24 @@
-import defaultAxios from 'axios';
-import { AWS_WEBSERVICE_ENDPOINT } from '@env';
+import defaultAxios from "axios";
+import { AWS_WEBSERVICE_ENDPOINT } from "@env";
 
 export interface ITesouro {
-    VALOR_VENDA: string;
-    INDEXADOR: string;
-    VENCIMENTO: string;
-    SELIC: string;
-    NOME: string;
-    VALOR_COMPRA: string;
-    ID: string;
-    RENT: string;
+  VALOR_VENDA: string;
+  INDEXADOR: string;
+  VENCIMENTO: string;
+  SELIC: string;
+  NOME: string;
+  VALOR_COMPRA: string;
+  ID: string;
+  RENT: string;
 }
 
-export const STATUS_INVEST_BASE_URL = 'https://statusinvest.com.br/';
-export const TESOURO_URL = 'tesouro/';
-export const ACOES_URL = 'acoes/';
-export const FII_URL = 'fiis/';
-export const CRIPTO_URL = 'cripto?ticker=';
+export const STATUS_INVEST_BASE_URL = "https://statusinvest.com.br/";
+export const TESOURO_URL = "tesouro/";
+export const ACOES_URL = "acoes/";
+export const FII_URL = "fiis/";
 
 const axios = defaultAxios.create({
-    baseURL: AWS_WEBSERVICE_ENDPOINT.toString()
+  baseURL: AWS_WEBSERVICE_ENDPOINT.toString(),
 });
 
 export default axios;
