8ffee38850050d3b14bcffb809b648411d20d559
DemetrioG <demetrio.gabriel@hotmail.com>
2022-09-01 12:30:38 -0300

    fix: ajuste no retorno de uid

--- src/utils/query.helper.ts
+++ src/utils/query.helper.ts
@@ -3,25 +3,27 @@ import { numberToReal } from "./number.helper";
 import { getStorage } from "./storage.helper";
 
 export const currentUser = async () => {
-  return firebase.auth().currentUser || (await getStorage("user"));
-}
+  return firebase.auth().currentUser || (await getStorage("authUser"));
+};
 
 /**
  * Dada a modalidade/mês retorna o saldo atual do usuário.
  */
- type TBalance = { month: string, modality: string }
- export const getBalance = async ({ month, modality }: TBalance) => {
-   const user = await currentUser();
- 
-   if (!user) return "R$ 0,00";
- 
-   const data = await firebase
-     .firestore()
-     .collection("balance").doc(user.uid)
-     .collection(modality).doc(month)
-     .get();
- 
-   const { balance } = data.data() as { balance: number };
- 
-   return balance ? numberToReal(balance) : "R$ 0,00";
- }
+type TBalance = { month: string; modality: string };
+export const getBalance = async ({ month, modality }: TBalance) => {
+  const user = await currentUser();
+
+  if (!user) return "R$ 0,00";
+
+  const data = await firebase
+    .firestore()
+    .collection("balance")
+    .doc(user.uid)
+    .collection(modality)
+    .doc(month)
+    .get();
+
+  const { balance } = data.data() as { balance: number };
+
+  return balance ? numberToReal(balance) : "R$ 0,00";
+};
