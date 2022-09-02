09eb994ec9203ca49f3de92113cc7b5fa6ebfb57
DemetrioG <demetrio.gabriel@hotmail.com>
2022-09-02 14:52:45 -0300

    fix: ajuste na importação do firebase

--- src/components/LineChart/query.ts
+++ src/components/LineChart/query.ts
@@ -1,4 +1,4 @@
-import firebase from "firebase";
+import firebase from "../../services/firebase";
 import { IData } from "../../context/Data/dataContext";
 import { getFinalDateMonth } from "../../utils/date.helper";
 import { currentUser } from "../../utils/query.helper";
