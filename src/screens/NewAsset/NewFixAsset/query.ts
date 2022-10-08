ef67ee7f59fd659b76028b0a70ba584fef6e74bd
DemetrioG <demetrio.gabriel@hotmail.com>
2022-10-07 23:21:48 -0300

    fix: query oara retorno de valores do tesouro

--- src/screens/NewAsset/NewFixAsset/query.ts
+++ src/screens/NewAsset/NewFixAsset/query.ts
@@ -1,21 +1,10 @@
 import firebase from "../../../services/firebase";
+import { IFixedIncome, ITreasure } from "../../../types/assets";
 import { convertDateToDatabase } from "../../../utils/date.helper";
 import { percentualToNumber, realToNumber } from "../../../utils/number.helper";
 import { createCollection } from "../../../utils/query.helper";
 
-export interface IAsset {
-  entrydate: string;
-  title: string | null;
-  cdbname: string | null;
-  broker: string | null;
-  rent: string;
-  rentType: string | null;
-  duedate: string | null;
-  price: string;
-  uid: string;
-}
-
-async function _registerAsset(props: IAsset) {
+async function _registerAsset(props: IFixedIncome) {
   const {
     entrydate,
     broker,
@@ -66,10 +55,33 @@ async function _registerAsset(props: IAsset) {
     .set(items);
 }
 
-export function registerAsset(props: IAsset) {
+async function _getTreasure() {
+  return firebase
+    .firestore()
+    .collection("treasure")
+    .get()
+    .then((v) => {
+      const data: ITreasure[] = [];
+      v.forEach((result) => {
+        data.push(result.data() as ITreasure);
+      });
+      return data;
+    });
+}
+
+export function registerAsset(props: IFixedIncome) {
   try {
     return _registerAsset(props);
   } catch (error) {
-    throw new Error("Erro");
+    throw new Error("Erro ao cadastrar Renda Fixa");
+  }
+}
+
+export function getTreasure() {
+  try {
+    return _getTreasure();
+  } catch (error) {
+    console.log(error);
+    throw new Error("Erro ao retornar os dados de Tesouro");
   }
 }
