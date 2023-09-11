45732b16a989243433968644fdba56664d17b5de
DemetrioG <gabriel.gauche@marvee.com.br>
2023-09-11 18:28:42 -0300

    feat: seletor de cores para contas

--- src/screens/App/Configurations/Records/Account/Form/index.tsx
+++ src/screens/App/Configurations/Records/Account/Form/index.tsx
@@ -1,16 +1,24 @@
-import { Keyboard } from "react-native";
-import { TouchableWithoutFeedback } from "react-native";
-import { BackgroundContainer } from "../../../../../../styles/general";
-import { Button, Center, HStack, Pressable, Text, VStack } from "native-base";
+import { useEffect, useState } from "react";
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
 import { useTheme } from "styled-components";
-import { ChevronLeft } from "lucide-react-native";
 import { useNavigation } from "@react-navigation/native";
+import { ChevronLeft, Palette } from "lucide-react-native";
+import { BackgroundContainer } from "../../../../../../styles/general";
+import {
+  Button,
+  Center,
+  HStack,
+  Pressable,
+  Text,
+  VStack,
+  useDisclose,
+} from "native-base";
 import { IThemeProvider } from "../../../../../../styles/baseTheme";
 import { FormTextInput } from "../../../../../../components/Inputs/TextInput";
 import { useDeleteAccount, useFormAccount } from "../hooks/useAccount";
 import { AccountFormParams } from "../types";
-import { useEffect } from "react";
 import When from "../../../../../../components/When";
+import ColorPicker from "react-native-wheel-color-picker";
 
 export const AccountForm = ({ route: { params } }: AccountFormParams) => {
   const id = params?.id;
@@ -19,6 +27,9 @@ export const AccountForm = ({ route: { params } }: AccountFormParams) => {
   const { formMethods, isLoadingCreate, isLoadingUpdate, handleSubmit } =
     useFormAccount(id);
   const { isLoadingDelete, handleDelete } = useDeleteAccount();
+
+  const [color, setColor] = useState("#266DD3");
+  const colorPicker = useDisclose();
 
   useEffect(() => {
     params && formMethods.reset(params);
@@ -41,15 +52,31 @@ export const AccountForm = ({ route: { params } }: AccountFormParams) => {
             <Text fontWeight={700}>Cadastrar Conta Bancária</Text>
           </HStack>
           <Center flex={1}>
-            <FormTextInput
-              variant="filled"
-              placeholder="Nome da conta"
-              control={formMethods.control}
-              name="name"
-              errors={formMethods.formState.errors.name}
-              helperText={formMethods.formState.errors.name?.message}
-              isRequired
-            />
+            <VStack space={5}>
+              <HStack justifyContent="space-between">
+                <HStack alignItems="center" space={3}>
+                  <Text>Cor de referência</Text>
+                  <VStack
+                    width="15px"
+                    height="15px"
+                    backgroundColor={color}
+                    borderRadius={10}
+                  />
+                </HStack>
+                <Pressable onPress={colorPicker.onToggle}>
+                  <Palette color={theme?.text} />
+                </Pressable>
+              </HStack>
+              <FormTextInput
+                variant="filled"
+                placeholder="Nome da conta"
+                control={formMethods.control}
+                name="name"
+                errors={formMethods.formState.errors.name}
+                helperText={formMethods.formState.errors.name?.message}
+                isRequired
+              />
+            </VStack>
             <FormTextInput
               variant="filled"
               name="balance"
@@ -60,6 +87,17 @@ export const AccountForm = ({ route: { params } }: AccountFormParams) => {
               helperText="Informe saldo inicial da conta"
               isRequired
             />
+            <When is={colorPicker.isOpen}>
+              <VStack height={100}>
+                <ColorPicker
+                  thumbSize={30}
+                  swatches={false}
+                  sliderHidden
+                  onColorChange={setColor}
+                  row={false}
+                />
+              </VStack>
+            </When>
           </Center>
           <When is={!id}>
             <Button onPress={handleSubmit} isLoading={isLoadingCreate}>
@@ -83,7 +121,7 @@ export const AccountForm = ({ route: { params } }: AccountFormParams) => {
             <Button
               variant="outline"
               isLoading={isLoadingDelete}
-              isDisabled={isLoadingUpdate || params.name === "Carteira"}
+              isDisabled={isLoadingUpdate || params?.name === "Carteira"}
               onPress={() => handleDelete(id)}
             >
               <Text fontWeight="bold" color={theme?.blue}>
